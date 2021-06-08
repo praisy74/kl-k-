68f667d2924d7ed83bf0ed0cf0aa34c9d0653236
praisy74 <76694676+praisy74@users.noreply.github.com>
2021-06-08 23:44:30 +0530

    Add files via upload

---
+++ Block.js
@@ -0,0 +1,35 @@
+class Block{
+  constructor(x, y, width, height) {
+      var options = {
+          restitution :0.4,
+          friction :0.0,
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image=(loadImage("block.png"))
+      World.add(world, this.body);
+      this.Visibility=255;
+    }
+   
+    display(){
+      var pos= this.body.position;
+      rectMode(CENTER);
+      rect(pos.x,pos.y,this.width, this.height);
+      imageMode(CENTER);
+      image(this.image, pos.x, pos.y, this.width, this.height)
+
+      if(this.body.speed<3){
+      this.body.Visibility=255
+      }else{
+        World.remove(world, this.body);
+  push()
+        this.Visibility=this.Visibility-5
+        tint(255,this.Visibility)
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop()
+      }
+    }
+   
+      
+}
