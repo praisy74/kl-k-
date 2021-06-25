29398b0655e38ee0c6d3180398b35d865ca2e62e
praisy74 <76694676+praisy74@users.noreply.github.com>
2021-06-25 12:48:51 +0530

    Add files via upload

--- Block.js
+++ Block.js
@@ -8,28 +8,30 @@ class Block{
       this.width = width;
       this.height = height;
       this.image=(loadImage("block.png"))
-      World.add(world, this.body);
       this.Visibility=255;
+      World.add(world, this.body);
     }
    
     display(){
-      var pos= this.body.position;
-      rectMode(CENTER);
-      rect(pos.x,pos.y,this.width, this.height);
-      imageMode(CENTER);
-      image(this.image, pos.x, pos.y, this.width, this.height)
+     // var pos= this.body.position;
+     // rectMode(CENTER);
+     // rect(pos.x,pos.y,this.width, this.height);
+     
 
-      if(this.body.speed<3){
+
+      if(this.body.speed<7){
+        imageMode(CENTER);
+        image(this.image, this.body.position.x, this.body.position.y, 30, 40);
       this.body.Visibility=255
       }else{
-        World.remove(world, this.body);
   push()
         this.Visibility=this.Visibility-5
-        tint(255,this.Visibility)
-     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+        tint(0,this.Visibility)
+     image(this.image, this.body.position.x, this.body.position.y, 30, 40);
      pop()
+        World.remove(world, this.body);
+
       }
     }
    
-      
 }
